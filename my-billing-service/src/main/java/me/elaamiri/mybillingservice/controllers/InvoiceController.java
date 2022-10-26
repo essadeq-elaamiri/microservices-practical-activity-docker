2596c38d91472a10d2d3418e44ff36eecb8beb34
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-26 22:34:46 +0100

    adding controller for billing-service

---
+++ my-billing-service/src/main/java/me/elaamiri/mybillingservice/controllers/InvoiceController.java
@@ -0,0 +1,57 @@
+package me.elaamiri.mybillingservice.controllers;
+
+import lombok.AllArgsConstructor;
+import me.elaamiri.mybillingservice.dtos.InvoiceRequestDTO;
+import me.elaamiri.mybillingservice.dtos.InvoiceResponseDTO;
+import me.elaamiri.mybillingservice.services.InvoiceService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api-v1/invoices")
+@AllArgsConstructor
+public class InvoiceController {
+
+    InvoiceService invoiceService;
+
+    // list pagination
+    @GetMapping("/")
+    public List<InvoiceResponseDTO> getInvoicesList(@RequestParam(defaultValue = "0") int page,
+                                                    @RequestParam(defaultValue = "10") int size){
+        return invoiceService.getInvoicesList(page, size);
+    }
+    // list customer invoices
+    @GetMapping("/customer/{customerId}")
+    public List<InvoiceResponseDTO> getCustomerInvoices(@PathVariable String customerId,
+                                                        @RequestParam(defaultValue = "0") int page,
+                                                        @RequestParam(defaultValue = "10") int size){
+        return invoiceService.getInvoicesListByCustomer(customerId, page, size);
+    }
+
+    // get one by id
+    @GetMapping("/{invoiceId}")
+    public InvoiceResponseDTO getInvoice(@PathVariable String invoiceId){
+        return invoiceService.getInvoiceById(invoiceId);
+    }
+    // insert
+
+    @PostMapping("/")
+    public InvoiceResponseDTO insertInvoice(@RequestBody @Valid InvoiceRequestDTO invoiceRequestDTO){
+        return invoiceService.saveInvoice(invoiceRequestDTO);
+    }
+
+    // update
+    @PostMapping("/{invoiceId}")
+    public InvoiceResponseDTO updateInvoice(@RequestBody @Valid InvoiceRequestDTO invoiceRequestDTO,
+                                            @PathVariable String invoiceId){
+        return invoiceService.updateInvoice(invoiceId, invoiceRequestDTO);
+    }
+    // delete
+    @PostMapping("/{invoiceId}")
+    public boolean deleteInvoice(@PathVariable String invoiceId){
+        return invoiceService.deleteInvoice(invoiceId);
+    }
+
+}
