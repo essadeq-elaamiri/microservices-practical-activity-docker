ee9d41ce75394798c7acd5258d640496e0e786c4
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-26 17:38:05 +0100

    init billing-service Maven lol the problem was with the gateway dependency hhh I think YEAH

---
+++ my-billing-service/src/main/java/me/elaamiri/mybillingservice/MyBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package me.elaamiri.mybillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBillingServiceApplication.class, args);
+    }
+
+}
---
+++ my-billing-service/src/test/java/me/elaamiri/mybillingservice/MyBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.mybillingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
