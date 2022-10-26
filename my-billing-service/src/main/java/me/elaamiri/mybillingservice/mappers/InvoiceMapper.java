632fc0d6f973944d0e2170e6e2c9759e4db36cea
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-26 20:34:55 +0100

    addint mapper mapstruct

--- my-billing-service/src/main/java/me/elaamiri/mybillingservice/mappers/InvoiceMapper.java
+++ my-billing-service/src/main/java/me/elaamiri/mybillingservice/mappers/InvoiceMapper.java
@@ -1,8 +1,12 @@
 package me.elaamiri.mybillingservice.mappers;
 
+import me.elaamiri.mybillingservice.dtos.InvoiceRequestDTO;
+import me.elaamiri.mybillingservice.dtos.InvoiceResponseDTO;
+import me.elaamiri.mybillingservice.entities.Invoice;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface InvoiceMapper {
-
+    InvoiceResponseDTO toInvoiceResponse(Invoice invoice);
+    Invoice toInvoice(InvoiceRequestDTO invoiceRequestDTO);
 }
