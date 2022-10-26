3b9e4d96637a4a1a05cdbc5993322bcf7721b54a
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-26 12:39:34 +0100

    init customer service

---
+++ customer-service/src/test/java/me/elaamiri/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
