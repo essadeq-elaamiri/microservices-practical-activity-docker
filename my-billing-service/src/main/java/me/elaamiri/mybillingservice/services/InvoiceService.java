8fead363754758826f6dddb28011fde7c0342645
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-26 20:38:40 +0100

    adding service interface

--- my-billing-service/src/main/java/me/elaamiri/mybillingservice/services/InvoiceService.java
+++ my-billing-service/src/main/java/me/elaamiri/mybillingservice/services/InvoiceService.java
@@ -1,5 +1,24 @@
 package me.elaamiri.mybillingservice.services;
 
+import me.elaamiri.mybillingservice.dtos.InvoiceRequestDTO;
+import me.elaamiri.mybillingservice.dtos.InvoiceResponseDTO;
+import me.elaamiri.mybillingservice.entities.Invoice;
+
+import java.util.List;
+
 public interface InvoiceService {
+
+    // list all
+    List<InvoiceResponseDTO> getInvoicesList(int page, int size);
+    // get one
+    InvoiceResponseDTO getInvoiceById(String id);
+    // insert
+    InvoiceResponseDTO saveInvoice(InvoiceRequestDTO invoiceRequestDTO);
+    // update
+    InvoiceResponseDTO updateInvoice(String id, InvoiceRequestDTO invoiceRequestDTO);
+    // delete
+    boolean deleteInvoice(String id);
+    // get customer's invoices
+    List<InvoiceResponseDTO> getInvoicesListByCustomer(String customerId);
 
 }
