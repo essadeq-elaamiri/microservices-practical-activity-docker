5935157270d1cb4361fa73871747d94f9461d5e0
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-28 23:47:52 +0100

    adding discovery service

---
+++ discovery-service/src/test/java/me/elaamiri/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
