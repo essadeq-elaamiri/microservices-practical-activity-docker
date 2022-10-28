15de419358a9a9197fc2849d00c001387902b0f4
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-29 00:31:02 +0100

    cnnected services

--- my-billing-service/src/main/java/me/elaamiri/mybillingservice/MyBillingServiceApplication.java
+++ my-billing-service/src/main/java/me/elaamiri/mybillingservice/MyBillingServiceApplication.java
@@ -1,8 +1,18 @@
 package me.elaamiri.mybillingservice;
 
+import me.elaamiri.mybillingservice.dtos.InvoiceRequestDTO;
+import me.elaamiri.mybillingservice.dtos.InvoiceResponseDTO;
+import me.elaamiri.mybillingservice.entities.Invoice;
+import me.elaamiri.mybillingservice.entities.helperModels.Customer;
+import me.elaamiri.mybillingservice.services.InvoiceService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 
 // Activating openFeign
@@ -14,4 +24,15 @@ public class MyBillingServiceApplication {
         SpringApplication.run(MyBillingServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(InvoiceService invoiceService){
+        return args -> {
+            String customerId = "96d2de30-a0dd-4243-8806-b6748e49e763";
+            InvoiceRequestDTO invoiceRequestDTO = new InvoiceRequestDTO();
+            invoiceRequestDTO.setAmount(BigDecimal.valueOf(17885.2));
+            invoiceRequestDTO.setCustomerId(customerId);
+            InvoiceResponseDTO invoice = invoiceService.saveInvoice(invoiceRequestDTO);
+
+        };
+    }
 }
