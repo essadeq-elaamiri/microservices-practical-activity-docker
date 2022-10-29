6aef9c0d5f9abc309fcb6dca1593424139dca45f
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-29 01:44:06 +0100

    gateway done

---
+++ gateway-service/src/main/java/me/elaamiri/gatewayservice/GatewayConfig.java
@@ -0,0 +1,16 @@
+package me.elaamiri.gatewayservice;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient reactiveDiscoveryClient,
+                                                                                DiscoveryLocatorProperties discoveryLocatorProperties){
+        return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
+    }
+}
--- gateway-service/src/main/java/me/elaamiri/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/me/elaamiri/gatewayservice/GatewayServiceApplication.java
@@ -10,4 +10,5 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+
 }
