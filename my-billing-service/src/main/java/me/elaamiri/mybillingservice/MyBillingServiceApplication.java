1cce79440599c8834410c6f8e7cbdcda5d66d47c
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-29 17:32:07 +0100

    jar files generating +

--- my-billing-service/src/main/java/me/elaamiri/mybillingservice/MyBillingServiceApplication.java
+++ my-billing-service/src/main/java/me/elaamiri/mybillingservice/MyBillingServiceApplication.java
@@ -24,17 +24,17 @@ public class MyBillingServiceApplication {
         SpringApplication.run(MyBillingServiceApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner start(InvoiceService invoiceService){
-        return args -> {
-            String customerId = "96d2de30-a0dd-4243-8806-b6748e49e763";
-            InvoiceRequestDTO invoiceRequestDTO = new InvoiceRequestDTO();
-            invoiceRequestDTO.setAmount(BigDecimal.valueOf(17885.2));
-            invoiceRequestDTO.setCustomerID(customerId);
-            System.out.println(customerId);
-            InvoiceResponseDTO invoice = invoiceService.saveInvoice(invoiceRequestDTO);
-
-            System.out.println(invoice);
-        };
-    }
+//    @Bean
+//    CommandLineRunner start(InvoiceService invoiceService){
+//        return args -> {
+//            String customerId = "96d2de30-a0dd-4243-8806-b6748e49e763";
+//            InvoiceRequestDTO invoiceRequestDTO = new InvoiceRequestDTO();
+//            invoiceRequestDTO.setAmount(BigDecimal.valueOf(17885.2));
+//            invoiceRequestDTO.setCustomerID(customerId);
+//            System.out.println(customerId);
+//            InvoiceResponseDTO invoice = invoiceService.saveInvoice(invoiceRequestDTO);
+//
+//            System.out.println(invoice);
+//        };
+//    }
 }
